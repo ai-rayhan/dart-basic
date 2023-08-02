b01024a1fad3e205ceac6429f47f44301c41726f
RayhanAhmed <rayhanahmed1127@gmail.com>
2023-08-02 23:20:58 +0600

    init repo

---
+++ lib/list_converting.dart
@@ -0,0 +1,42 @@
+// List complexList = [
+//   'mylist',
+//   20,
+//   PDF(id: "id", title: "title56", code: "code"),
+//   ['name', 4988],
+// ];
+
+// findDataFromComplexListAfterMapConverting() {
+//   Map<int, dynamic> map = complexList.asMap();
+
+//   PDF findfromlistAnObject = map[2];
+//   List findfromlistAnObject2 = map[3];
+//   print(findfromlistAnObject.title);
+//   print(findfromlistAnObject2[1]);
+// }
+
+// findDataFromComplexList() {
+//   PDF findfromlistAnObject = complexList[2];
+//   List findfromlistAnObject2 = complexList[3];
+//   print(findfromlistAnObject.title);
+//   print(findfromlistAnObject2[0]);
+//   Map<int, dynamic> listtomap1 = complexList.asMap();
+// }
+
+// List<PDF> PDFs = [
+//   PDF(id: 'id', title: 'title', code: 'code'),
+//   PDF(id: 'id1', title: 'title1', code: 'code1'),
+//   PDF(id: 'id2', title: 'title2', code: 'code2'),
+//   PDF(id: 'id3', title: 'title3', code: 'code3'),
+// ];
+
+// class PDF {
+//   String id;
+//   String title;
+//   String code;
+
+//   PDF({
+//     required this.id,
+//     required this.title,
+//     required this.code,
+//   });
+// }
