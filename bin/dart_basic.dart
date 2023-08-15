0eb594cb5b896bfec746b905c0873d506465dee4
RayhanAhmed <rayhanahmed1127@gmail.com>
2023-08-15 21:32:24 +0600

    main update

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,18 +1,10 @@
+import 'package:dart_basic/assainment/assainment2.dart';
 import 'package:dart_basic/live_test/live_test2.dart';
-import 'package:dart_basic/practices/practice.dart';
+import 'package:dart_basic/problem_solving/problem_solving1.dart';
 
 void main() {
-  Base().baseMathod();
-  // List<double> itemPrices = [12.99, 8.75, 21.50, 5.00];
-  // double taxRate = 0.08;
-
-  // double subtotal = 0;
-  // for (double price in itemPrices) {
-  //   subtotal += price;
-  // }
-
-  // double taxAmount = subtotal * taxRate;
-  // double totalCost = subtotal + taxAmount;
-
-  // print("Total Cost: \$${totalCost.toStringAsFixed(2)}");
+  livetest2();
+  assainment2();
+  problem3();
+  problem4();
 }
